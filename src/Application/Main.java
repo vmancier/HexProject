7ec37f5a2a773f23ed21d8e28877455369d32cab
vmancier <mancier.v33@gmail.com>
2015-10-14 14:44:35 +0200

    finalisation du Main Signed-off-by: vmancier

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,11 +1,18 @@
 package Application;
 
+import Controller.*;
+import Model.*;
+import View.*;
+
 /**
  * Created by Valentin on 14/10/2015.
  */
 public class Main {
     public Main() {
-
+        HexModel hm = new HexModel();
+        HexController hc = new HexController(hm);
+        HexView hv = new HexView("Hex Project", hm, hc, 200, 200);
+        hc.addView(hv);
     }
 
     public static void main(String args[]) {
--- src/Controller/HexController.java
+++ src/Controller/HexController.java
@@ -1,7 +1,21 @@
 package Controller;
 
+import Model.*;
+import View.*;
+
 /**
  * Created by Valentin on 14/10/2015.
  */
 public class HexController {
+    private HexModel model;
+    private HexView view = null;
+
+    public HexController(HexModel m){
+        model =m;
+    }
+
+    public void addView(HexView view)
+    {
+        this.view = view;
+    }
 }
